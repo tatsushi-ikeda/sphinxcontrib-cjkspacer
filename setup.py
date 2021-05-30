8e78d71908604e513d2972e19aa2cc27b68960f5
Tatsushi Ikeda <ikeda.tatsushi.37u@kyoto-u.jp>
2021-05-30 12:58:56 +0900

    Update README.md

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def _requires_from_file(filename):
 
 setup(
     name='sphinxcontrib-cjkspacer',
-    version='0.4.0',
+    version='0.4.1',
     url='https://github.com/tatsushi-ikeda/sphinxcontrib-cjkspacer',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-cjkspacer',
     license='MIT',
